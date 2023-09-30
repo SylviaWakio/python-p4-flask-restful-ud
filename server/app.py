4d6baca3e6334eef56f3d98a8d0ba9b6bd7da49d
+ <+>
2023-09-30 04:24:31 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask import Flask, jsonify
+
 
 from models import db, Newsletter
 
@@ -78,6 +80,45 @@ class NewsletterByID(Resource):
             200,
         )
 
+        return response
+    
+    def patch(self, id):
+        
+        record = Newsletter.query.filter_by(id=id).first()
+        
+#Looping through the form data gives us its keys, the attribute names to be changed        
+        for attr in request.form:
+            
+#we can set each attribute on the Newsletter object to its new value with setattr()            
+            setattr(record, attr, request.form[attr])
+            
+            db.session.add(record)
+            db.session.commit()
+            
+            response_dict = record.to_dict()
+            
+            response = make_response(
+                jsonify(response_dict),
+                200
+            )
+            
+            return response
+        
+        
+    def delete(self, id):
+            
+        record = Newsletter.query.filter_by(id=id).first()
+            
+        db.session.delete(record)
+        db.session.commit()
+        
+        response_dict = {"message": "record successfully deleted"}
+        
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+        
         return response
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
